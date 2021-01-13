79906625e7c2585cd740632e7f262d57325b548f
ThanGrove <than@virginia.edu>
2021-01-13 13:40:15 -0500

    Updates to code for cleaning ocr and making a docs folder

--- clean-ocr.py
+++ clean-ocr.py
@@ -1,4 +1,51 @@
-import os
-import re
+# Script to clean up OCR
 
-file_in = ''
+file_in = 'docs/filein.txt'
+file_out = 'docs/fileout.txt'
+
+margin = 'རྙིང་རྒྱུད།'
+tibvol = 'འ '
+newpage = True
+premargin = True
+pgtxt = ''
+pgnum = 600
+lnnum = 1
+
+with open(file_in, 'r') as fin, open(file_out, 'w') as fout:
+    fout.write("<div>\n")
+    for ln in fin.readlines():
+        ln = ln.strip()
+        if ln.isnumeric() and len(ln) == 3:
+            if len(pgtxt) > 0:
+                fout.write(pgtxt)
+                pgtxt = ''
+            elif pgnum > 0:
+                print("**** Page {} is blank! ****".format(pgnum))
+            pgnum = int(ln) + 1
+            lnnum = 1
+            print("Page {}".format(pgnum))
+            newpage = True
+            premargin = True
+            pgtxt += '<milestone unit="page" n="{0}"/>'.format(pgnum)
+            continue
+        elif ln == '༄༅།':
+            continue
+        elif ln == margin or ln.startswith(tibvol):
+            print("margin: {}".format(ln))
+            premargin = False
+        elif not premargin and len(ln) > 0:
+            if lnnum == 1:
+                ln = ln[1:] if ln[0] == '།' else ln
+                pgtxt += '<milestone unit="line" n="{}.{}"/>'.format(pgnum, lnnum)
+                lnnum += 1
+            elif ln[0] != '།':
+                pgtxt += '<milestone unit="line" n="{}.{}"/>'.format(pgnum, lnnum)
+                lnnum += 1
+
+            pgtxt += ln.replace('༄༅། ', '')
+
+    fout.write(pgtxt)
+    fout.write("\n</div>")
+
+print("Done!")
+
--- quickocr.py
+++ quickocr.py
@@ -2,11 +2,11 @@ from google.cloud import vision
 
 client = vision.ImageAnnotatorClient()
 
-with open('ocr-out.txt', 'a+', encoding='utf-8') as outf:
-    for n in range(0, 100):
-        txtnum = 540 + n
+with open('ocr-out-p600.txt', 'a+', encoding='utf-8') as outf:
+    for n in range(0, 1):
+        txtnum = 600  # 540 + n
         print("doing page {}".format(txtnum))
-        imgurl = 'gs://ngb-peltsek-scans/inverted/out_0{}-inv.jpg'.format(txtnum)
+        imgurl = 'gs://ngb-peltsek-scans/inverted/out_0{}.jpg'.format(txtnum)
         # print(imgurl)
         response = client.document_text_detection({
             'source': {'image_uri': imgurl},
